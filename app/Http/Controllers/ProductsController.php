5f02784796d04aef38e472a12ae7a856269db40d
Hafiz <m.hafiz1825@gmail.com>
2021-09-10 12:55:33 +0000

    fix commit

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,15 +10,11 @@ class ProductsController extends Controller
     public function productDetail($slug)
     {
         $product = Products::where('slug', $slug)->firstOrFail();
-<<<<<<< HEAD
-        $related = Products::where('slug', '!=', $slug)->take(4)->get();
-=======
         $related = Products::where('slug', '!=', $slug)->where('category', 'service')->take(4)->get();
 
         if($product->category == 'product') {
             $related = Products::where('slug', '!=', $slug)->where('category', 'product')->take(4)->get();
         }
->>>>>>> 9b1c29099e6cd54c0874a3d75fd3036f272c4875
         return view('product-detail', compact('product', 'related'));
     }
 }
