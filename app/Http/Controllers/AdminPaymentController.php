77de59346443b8adbd1414490b7357907dc0f48f
Muhamad Hafiz <m.hafiz1825@gmail.com>
2024-09-11 08:26:07 +0700

    order by created at

--- app/Http/Controllers/AdminPaymentController.php
+++ app/Http/Controllers/AdminPaymentController.php
@@ -12,7 +12,7 @@ class AdminPaymentController extends Controller
     public function index()
     {
         $sales = Sales::where('status', 'pending')->orWhere('status', 'paid')->with('additional')
-            ->orderBy('status', 'asc')->get();
+            ->orderBy('created_at')->get();
 
         return view('admin.payment.index', compact('sales'));
     }
