a8f943f08cbee1a342ca033627acdb20a7371407
Muhamad Hafiz <m.hafiz1825@gmail.com>
2025-08-07 11:18:59 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -105,9 +105,11 @@
             <section class="section-side">
                 <a href="https://shope.ee/3pzPWRt09j" target="_blank" class="index-small">
                     <section class="index-section index-article">
-                        @if ($perfume && $perfume->image)
-                            <img src={{ Storage::url('perfume-image/'.$perfume->image) }} alt="">
-                        @endif
+                        <div class="bg">
+                            @if ($perfume && $perfume->image)
+                                <img src={{ Storage::url('perfume-image/'.$perfume->image) }} alt="">
+                            @endif
+                        </div>
                     </section>
                 </a>
                 <a href="{{ route('workshops') }}" class="index-kelas">
